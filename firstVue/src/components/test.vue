553e700199fd99495b5b82b88891884e49eadc25
Jiachenglong2518 <251895921@qq.com>
2018-05-25 17:18:20 +0800

    'ss'

---
+++ firstVue/src/components/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='test1'>
+    <h1 class='a' v-on:click='alert1'>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'My name is Jiachenglong HHHH'
+    }
+  },
+  created: function () {
+    console.log(1)
+    console.log(this.msg)
+  },
+  mounted: function () {
+    console.log(window)
+  },
+  methods: {
+    alert1: function () {
+      alert(1)
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+    .test1{
+        .a{
+            color: red;
+        }
+   }
+</style>
